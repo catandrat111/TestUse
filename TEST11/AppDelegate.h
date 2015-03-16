4e4f26a9cc3af6adf97018c045d87792d9d92b2e
catandrat111 <catandrat111@126.com>
2015-03-16 20:22:34 +0800

    Initial Commit

---
+++ TEST11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEST11
+//
+//  Created by 董金亮 on 15-3-16.
+//  Copyright (c) 2015年 董金亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEST11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST11
+//
+//  Created by 董金亮 on 15-3-16.
+//  Copyright (c) 2015年 董金亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
