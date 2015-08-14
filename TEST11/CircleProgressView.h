a2756ada3672eff5801d30f8d7302cd69acb7ef0
catandrat111 <catandrat111@126.com>
2015-08-14 23:19:27 +0800

    gesture

---
+++ TEST11/CircleProgressView.h
@@ -0,0 +1,13 @@
+//
+//  CircleProgressView.h
+//  TEST11
+//
+//  Created by 董金亮 on 15/8/14.
+//  Copyright (c) 2015年 djl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleProgressView : UIView
+
+@end
---
+++ TEST11/CircleProgressView.m
@@ -0,0 +1,21 @@
+//
+//  CircleProgressView.m
+//  TEST11
+//
+//  Created by 董金亮 on 15/8/14.
+//  Copyright (c) 2015年 djl. All rights reserved.
+//
+
+#import "CircleProgressView.h"
+
+@implementation CircleProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
