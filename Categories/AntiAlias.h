fe29dab2e837237203b214a52165f069b60eb7fd
catandrat111 <catandrat111@126.com>
2015-09-15 17:44:52 +0800

    15-09-15 17.44

---
+++ Categories/AntiAlias.h
@@ -0,0 +1,13 @@
+//
+//  AntiAlias.h
+//  TEST11
+//
+//  Created by dongjl on 15-9-15.
+//  Copyright (c) 2015年 djl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AntiAlias : UIImage
+- (UIImage *)antiAlias;
+@end
---
+++ Categories/AntiAlias.m
@@ -0,0 +1,30 @@
+//
+//  AntiAlias.m
+//  TEST11
+//
+//  Created by dongjl on 15-9-15.
+//  Copyright (c) 2015年 djl. All rights reserved.
+//
+
+#import "AntiAlias.h"
+
+@implementation AntiAlias
+- (UIImage *)antiAlias
+{
+    CGFloat border = 1.0f;
+    CGRect rect = CGRectMake(border, border, self.size.width-2*border, self.size.height-2*border);
+    UIImage *img = nil;
+    
+    UIGraphicsBeginImageContext(CGSizeMake(rect.size.width,rect.size.height));
+    [self drawInRect:CGRectMake(-1, -1, self.size.width, self.size.height)];
+    img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    UIGraphicsBeginImageContext(self.size);
+    [img drawInRect:rect];
+    UIImage* antiImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return antiImage;
+}
+@end
